23947941d114abbcd445e99e31067ae793ecb03f
vsRushy <gerardmarcosfreixasgmail.com>
2018-11-23 23:28:40 +0100

    Camera smooth follow

---
+++ IDI_game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+    public float smoothTimeX;
+    public float smoothTimeY;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
