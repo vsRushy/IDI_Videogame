b7bf7507441099a0db774276b54ca636d57e4899
vsRushy <gerardmarcosfreixasgmail.com>
2018-11-25 12:30:41 +0100

    Added player light

---
+++ IDI_game/Assets/Scripts/PlayerLightPos.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerLightPos : MonoBehaviour
+{
+    public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
