ac6d92193efc7db3e184bef7f5f61c0346e6352f
vsRushy <vsRushy@users.noreply.github.com>
2018-11-28 21:50:24 +0100

    Pause in-game

---
+++ IDI_game/Assets/TimePause.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TimePause : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+            else Time.timeScale = 1;
+        }
+	}
+}
