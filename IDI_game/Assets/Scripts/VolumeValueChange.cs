5ded1e03b8d735684c4b340c0d6391ce86538608
vsRushy <vsRushy@users.noreply.github.com>
2018-11-29 23:59:05 +0100

    Updated menu + change volume main menu

---
+++ IDI_game/Assets/Scripts/VolumeValueChange.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class VolumeValueChange : MonoBehaviour
+{
+    private AudioSource music;
+
+    private float music_volume = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        music = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        music.volume = music_volume;
+	}
+
+    public void SetVolume(float volume)
+    {
+        music_volume = volume;
+    }
+}
