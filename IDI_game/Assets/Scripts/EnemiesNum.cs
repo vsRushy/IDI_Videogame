7dbfd64142e6f366c65a5b5cfc1078daf9b80c01
vsRushy <vsRushy@users.noreply.github.com>
2018-12-03 00:22:19 +0100

    Difficulty (only visual), choose number of enemies

---
+++ IDI_game/Assets/Scripts/EnemiesNum.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemiesNum : MonoBehaviour
+{
+    public InputField input_field;
+
+    public static int enemies_number;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        enemies_number = int.Parse(input_field.text.ToString());
+	}
+}
