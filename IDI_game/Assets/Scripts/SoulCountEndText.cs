110cdce9660f3fa7d92fba46c04bfa199037ca0b
vsRushy <vsRushy@users.noreply.github.com>
2018-12-03 23:54:24 +0100

    Showing final souls number at end

---
+++ IDI_game/Assets/Scripts/SoulCountEndText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class SoulCountEndText : MonoBehaviour
+{
+    public TMP_Text soulEndCountText;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        soulEndCountText.text = PlayerMovement.final_souls.ToString();
+    }
+}
