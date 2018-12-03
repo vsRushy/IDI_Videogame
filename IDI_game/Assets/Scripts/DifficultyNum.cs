600ba768e9270b60b84913acb87168912b0ee8e3
vsRushy <vsRushy@users.noreply.github.com>
2018-12-03 01:08:50 +0100

    Difficulty working (reduces more or less the size of the player)

---
+++ IDI_game/Assets/Scripts/DifficultyNum.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyNum : MonoBehaviour
+{
+    public Slider dif_slider;
+
+    public static int difficulty;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        difficulty = (int)dif_slider.value;
+    }
+}
--- IDI_game/Assets/Scripts/TimerPlayer.cs
+++ IDI_game/Assets/Scripts/TimerPlayer.cs
@@ -1,10 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerPlayer : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
+    private int difficulty;
+    private float reducing_time;
+
+    // Use this for initialization
+    void Start ()
     {
+        difficulty = DifficultyNum.difficulty;
+        switch (difficulty)
+        {
+            case 1:
+                reducing_time = 0.0005f;
+                break;
+            case 2:
+                reducing_time = 0.001f;
+                break;
+            case 3:
+                reducing_time = 0.008f;
+                break;
+            default:
+                break;
+        }
+
         InvokeRepeating("ReduceScale", 0.0f, 4.0f);
     }
 	
@@ -16,7 +36,7 @@ public class TimerPlayer : MonoBehaviour
 
     void ReduceScale()
     {
-        transform.localScale = new Vector3(transform.localScale.x - 0.008f, transform.localScale.y - 0.008f, transform.localScale.z);
+        transform.localScale = new Vector3(transform.localScale.x - reducing_time, transform.localScale.y - reducing_time, transform.localScale.z);
     }
 }
 
