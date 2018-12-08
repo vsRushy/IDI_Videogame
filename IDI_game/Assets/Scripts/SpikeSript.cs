369961dd6ec6b3c05807f8a572dc979d8b45e119
vsRushy <gerardmarcosfreixasgmail.com>
2018-12-08 18:28:43 +0100

    Provisional spikes (still no movement, collision control))

---
+++ IDI_game/Assets/Scripts/SpikeSript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpikeSript : MonoBehaviour
+{
+    public GameObject spike;
+    public uint num_spikes;
+
+	// Use this for initialization
+	void Start ()
+    {
+		for(int i = 0; i < num_spikes; i++)
+        {
+            Instantiate(spike, new Vector3(Random.Range(-18.5f, 18.5f), Random.Range(-10.0f, 10.0f), -1), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
