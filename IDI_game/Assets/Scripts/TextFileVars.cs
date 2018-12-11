c6978fb2950580be33057981203c6631d6dd5b7c
GerardMF21 <32986524+vsRushy@users.noreply.github.com>
2018-12-11 13:28:54 +0100

    Testing text

---
+++ IDI_game/Assets/Scripts/TextFileVars.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TextFileVars : MonoBehaviour
+{
+
+    int speed = 5;
+
+    string[] lines = { "Player Speed: ", "", "Number of seeds: ", "", "Player Color: ", "", "Food color: " };
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void SetVariables()
+    {
+
+        lines[1] = speed.ToString();
+        System.IO.File.WriteAllLines(@"C:/Users/gerardmf2/Documents/GitHub/IDI_Videogame/IDI_game/Assets/variables.txt", lines);
+    }
+}
