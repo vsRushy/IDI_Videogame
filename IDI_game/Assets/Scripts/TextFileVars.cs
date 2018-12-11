ed75952e34c9e6546e41aafe950b26b55e4e8df8
vsRushy <vsRushy@users.noreply.github.com>
2018-12-11 23:50:13 +0100

    Done

--- IDI_game/Assets/Scripts/TextFileVars.cs
+++ IDI_game/Assets/Scripts/TextFileVars.cs
@@ -7,13 +7,19 @@ public class TextFileVars : MonoBehaviour
     string souls_collected = "";
     string player_color = "";
     string spikes_color = "";
+    string game_end = "";
+    string minutes_played = "";
+    string seconds_played = "";
+    string difficulty = "";
 
     string[] lines = { "Player Speed: ", "",
         "Number of souls generated: ", "",
         "Number of souls collected: ", "",
         "Player color: ", "",
         "Spikes color: ", "",
-        "Win(1) / Lose(0):", ""
+        "Win(1) / Lose(2):", "",
+        "Time played:", "",
+        "Difficulty[1: easy, 2: normal, 3: hard]: ", ""
     };
 
     public void SetVariables()
@@ -32,6 +38,16 @@ public class TextFileVars : MonoBehaviour
 
         spikes_color = SpikeMovement.spike_color;
         lines[9] = this.spikes_color;
+
+        game_end = TimerPlayer.game_end.ToString();
+        lines[11] = this.game_end;
+
+        minutes_played = Timer.minutes.ToString();
+        seconds_played = Timer.seconds.ToString();
+        lines[13] = "Minutes: " + this.minutes_played + " Seconds: " + this.seconds_played;
+
+        difficulty = DifficultyNum.difficulty.ToString();
+        lines[15] = this.difficulty;
 
         System.IO.File.WriteAllLines(@"C:\Users\usuario\Documents\GitHub\IDI_Videogame\IDI_game\Assets\variables.txt", lines);
     }
