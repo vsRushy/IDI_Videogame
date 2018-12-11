d0260a05dd5e806b80326aab37d3ac3673b85e70
vsRushy <vsRushy@users.noreply.github.com>
2018-12-11 01:39:14 +0100

    Difficulty affects spikes

--- IDI_game/Assets/Scripts/SpikeMovement.cs
+++ IDI_game/Assets/Scripts/SpikeMovement.cs
@@ -2,12 +2,36 @@
 
 public class SpikeMovement : MonoBehaviour
 {
+    private int difficulty;
+
     public Rigidbody2D rb;
 
-    public float accelerationTime = 2f;
+    public float accelerationTime;
     public float maxSpeed = 5f;
     private Vector2 movement;
     private float timeLeft;
+
+    void Start()
+    {
+        difficulty = DifficultyNum.difficulty;
+        switch(difficulty)
+        {
+            case 1:
+                accelerationTime = 5f;
+                maxSpeed = 8f;
+                break;
+            case 2:
+                accelerationTime = 9f;
+                maxSpeed = 12f;
+                break;
+            case 3:
+                accelerationTime = 11f;
+                maxSpeed = 16f;
+                break;
+            default:
+                break;
+        }
+    }
 
     void Update()
     {
