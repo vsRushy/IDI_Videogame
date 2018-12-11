fc64df18fef29a678ed0cb3d490b0e0df0ebcfa5
GerardMF21 <32986524+vsRushy@users.noreply.github.com>
2018-12-11 13:49:53 +0100

    Souls generated, souls collected text

--- IDI_game/Assets/Scripts/TextFileVars.cs
+++ IDI_game/Assets/Scripts/TextFileVars.cs
@@ -2,25 +2,26 @@
 
 public class TextFileVars : MonoBehaviour
 {
-
     string speed = "";
+    string souls_generated = "";
+    string souls_collected = "";
 
-    string[] lines = { "Player Speed: ", "", "Number of seeds: ", "", "Player Color: ", "", "Food color: " };
-
-	void Start ()
-    {
-		
-	}
-	
-	void Update ()
-    {
-		
-	}
+    string[] lines = { "Player Speed: ", "",
+        "Number of souls generated: ", "",
+        "Number of souls collected: ", "",
+        "Player Color: ", "", "Food color: "
+    };
 
     public void SetVariables()
     {
         speed = PlayerMovement.speed.ToString();
         lines[1] = this.speed;
+
+        souls_generated = EnemiesNum.enemies_number.ToString();
+        lines[3] = this.souls_generated;
+
+        souls_collected = PlayerMovement.final_souls.ToString();
+        lines[5] = this.souls_collected;
 
         System.IO.File.WriteAllLines(@"C:/Users/gerardmf2/Documents/GitHub/IDI_Videogame/IDI_game/Assets/variables.txt", lines);
     }
