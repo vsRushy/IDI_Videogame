67ccf19d552983cea2b1e2c95380f62cdb31a00c
GerardMF21 <32986524+vsRushy@users.noreply.github.com>
2018-12-11 13:02:13 +0100

    Speed in game

---
+++ IDI_game/Assets/Scripts/SpeedText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedText : MonoBehaviour
+{
+    public Text speedText;
+
+    public PlayerMovement player_movement;
+
+    // Use this for initialization
+    void Start()
+    {
+        player_movement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        speedText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedText.text = "Speed: " + player_movement.speed.ToString();
+    }
+}
